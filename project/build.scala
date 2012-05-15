39343d77d843b69a458e6fd7ea5eeced9df438cc
Cyril Nikolaev <cyril@nichtverstehen.de>
2012-05-16 03:54:14 +0400

    Add local unfiltered-scalate as heroku fails to clone git.

--- project/build.scala
+++ project/build.scala
@@ -5,5 +5,5 @@ object MyApp extends Build
   lazy val root =
     Project("root", file(".")) dependsOn(unfilteredScalate)
   lazy val unfilteredScalate =
-    uri("git://github.com/unfiltered/unfiltered-scalate#5f1a99c2bca9cb78bb6843003c57b4081de8dc91")
+    file("unfiltered-scalate")
 }
