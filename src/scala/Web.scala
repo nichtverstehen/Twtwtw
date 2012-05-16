041d086e5761cffe7635e516b1b65ae40e04a668
Cyril Nikolaev <cyril@nichtverstehen.de>
2012-05-16 06:31:26 +0400

    Add jetty libs to idea. Move Web.scala to scala subdir.

--- src/scala/Web.scala
+++ src/scala/Web.scala
@@ -4,15 +4,17 @@ import unfiltered.jetty._
 import unfiltered.scalate.Scalate
 import org.fusesource.scalate.TemplateEngine
 
+object Handlers {
+  val intent = unfiltered.Cycle.Intent[Any, Any] {
+    case _ => ResponseString("Hello")
+  }
+}
+
 object Web {
   def main(args: Array[String]) {
     val engine = new TemplateEngine(List(new java.io.File("templates")))
 
-    Http(8080).context("/public"){ ctx: ContextBuilder =>
-      ctx.resources(new java.net.URL("file:///Users/molecule/development/scalate_demo/src/main/resources/public"))
-    }.filter(unfiltered.filter.Planify {
-      case req => Ok ~> Scalate(req, "hello.ssp")(engine)
-    }).run
+    Http(8080).filter(unfiltered.filter.Planify(Handlers.intent)).run
   }
 
 }
